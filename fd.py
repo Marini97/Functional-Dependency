5b059310804254e2fb90b76d161caa2f5c5c0063
Valerio Marini <marinivalerio97@gmail.com>
2023-03-24 16:23:25 +0100

    FDs are now grupped by determinants

--- fd.py
+++ fd.py
@@ -4,14 +4,80 @@ import pandas as pd
 import subprocess
 import jsonlines
 from io import StringIO
+from PIL import Image
 # to run this app, run the following command in the terminal
 # streamlit run fd.py
 
-
+# cache the uploaded dataset to avoid reading it every time the app is updated
 @st.cache_data
 def get_data(file):
     return pd.read_csv(file)
 
+# the dataset parameter is used to check if the dataset has been changed
+# if it has been changed, then we need to read the output file again
+# if it hasn't been changed, then we can use the cached results
+@st.cache_data
+def read_output(dataset, results, fd):
+    try:
+        with jsonlines.open('results/data_fds') as reader:
+            df_num = len(df.columns)
+            for data in reader:
+                
+                determinant = data['determinant']['columnIdentifiers']
+                for i in range(len(determinant)):
+                    determinant[i] = determinant[i]['columnIdentifier']
+                    
+                dependant = data['dependant']['columnIdentifier']
+                
+                if len(determinant)+1 < df_num:
+                    score = df[determinant+[dependant]].drop_duplicates().shape[0]
+                    results.append({'determinants': determinant, 'dependant': dependant, 'score': score})
+        
+    except Exception as exception:
+        fd = False
+
+    if len(results) == 0:
+        fd = False
+        st.write("The **HyFD Algorithm** didn't find any **Functional Dependencies** in this dataset.")
+        
+    return results, fd
+    
+        # create a dataframe with all FDs
+    rows = pd.DataFrame(columns=['Determinants', 'Dependant', 'Score'])
+    for result in results:
+        determinants = ", ".join(map(str,result['determinants']))
+        dependant = result['dependant']
+        score = result['score']
+        row = pd.DataFrame({'Determinants': [determinants], 'Dependant': [dependant], 'Score': [score]})
+        rows = pd.concat([rows, row], ignore_index=True)
+    return rows
+
+# get all FDs and group them by Determinants
+@st.cache_data
+def get_fds(results, df_num):
+    rows = pd.DataFrame(columns=['Determinants', 'Dependant', 'Score'])
+    for result in results:
+        determinants = ", ".join(map(str,result['determinants']))
+        dependant = result['dependant']
+        score = result['score']
+        row = pd.DataFrame({'Determinants': [determinants], 'Dependant': [dependant], 'Score': [score]})
+        rows = pd.concat([rows, row], ignore_index=True)
+        
+    # group the FDs by Determinants and concatenate the Dependant attributes
+    rows = rows.groupby('Determinants').agg({'Dependant': ', '.join, 'Score': 'mean'}).reset_index()
+    # if the union of Determinants and Dependant contains all attributes, then the FD is filtered out
+    for row in rows.itertuples():
+        if len(row.Determinants.split(", "))+len(row.Dependant.split(", ")) > df_num:
+            rows = rows.drop(row.Index)
+    return rows
+
+icon = Image.open('icon.png')
+st.set_page_config(
+    page_title="Functional Dependency App",
+    page_icon=icon,
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 # title of the app
 st.title("Functional Dependency App\n\n")
 
@@ -23,17 +89,18 @@ with st.sidebar:
     df = sns.load_dataset(dataset)
 
     # upload a dataset
-    uploaded_file = st.file_uploader("or upload a CSV file from your computer")
+    uploaded_file = st.file_uploader("Or upload a CSV file from your computer.")
     if uploaded_file is not None and not uploaded_file.name.endswith(".csv"):
         st.error("You need to select a CSV file.")
         uploaded_file = None
     if uploaded_file is not None:
         try:
             df = get_data(file=uploaded_file)
+            dataset = uploaded_file.name
         except(pd.errors.EmptyDataError):
             st.error("The selected CSV file is empty.")
             uploaded_file = None
-        
+            
 # write the selected dataset to a csv file 
 if df is not None:
     # remove all files from the results folder
@@ -42,61 +109,36 @@ if df is not None:
     df.to_csv("results/data.csv", index=False)
     st.header("Selected Dataset")
     st.dataframe(df, use_container_width=True)
-
-# run Metanome through subprocess.run:
+    
+# run Metanome through subprocess.run
+# if the dataset doesn't have any FDs, then the output file will be empty
 subprocess.run(["java", "-cp", "jars/*", "de.metanome.cli.App", "--algorithm", "de.metanome.algorithms.hyfd.HyFD", 
                 "--file-key", "INPUT_GENERATOR", "--files", "results/data.csv", "--separator", ",", 
                 "--header", "-o", "file:data"])
 
-# if the dataset doesn't have any FDs, then it will not create a result file
-# so we need to check if the result file exists
-# if it doesn't exist, then we need to display a message
 results = []
 fd = True
+results, fd = read_output(dataset, results, fd)
 
-try:
-    with jsonlines.open('results/data_fds') as reader:
-        df_num = len(df.columns)
-        for data in reader:
-            
-            determinant = data['determinant']['columnIdentifiers']
-            for i in range(len(determinant)):
-                determinant[i] = determinant[i]['columnIdentifier']
-                
-            dependant = data['dependant']['columnIdentifier']
-            
-            if len(determinant)+1 < df_num:
-                score = df[determinant+[dependant]].drop_duplicates().shape[0]
-                results.append({'determinants': determinant, 'dependant': dependant, 'score': score})
-                
-    if len(results) == 0:
-        fd = False
-        st.write("There are no **Functional Dependencies** in this dataset.")
-except:
-    fd = False
-
-# write the results to the streamlit app
-# in a table like format
+# write the results to the streamlit app 
 if fd:
     st.header("Functional Dependencies")
-    st.write("The score is the number of **unique tuples** for each FD."
-            +"If the union of Determinants and Dependant **contains all attributes** then the FD is filtered out.")
+    st.write("In the following dropdown menu you can see all the **Functional Dependencies** found in the dataset. " 
+             +"The **Functional Dependencies** are grouped by **Determinants** and sorted by their unique number of rows. "
+             +"An FD is filtered out if the union of Determinants and Dependants **contains all the attributes**.")
     
-    # create a dataframe with all FDs
-    rows = pd.DataFrame(columns=['Determinants', 'Dependant', 'Score'])
-    for result in results:
-        determinants = ", ".join(map(str,result['determinants']))
-        dependant = result['dependant']
-        score = result['score']
-        row = pd.DataFrame({'Determinants': [determinants], 'Dependant': [dependant], 'Score': [score]})
-        rows = pd.concat([rows, row], ignore_index=True)
-        
+    df_num = len(df.columns)
+    st.write("The dataset has **"+str(df_num)+" columns**. The HyFD algorithm "
+             +"Found "+str(len(results))+" **Functional Dependencies**")
+    rows = get_fds(results, df_num)    
     # sort the FDs by score
     rows = rows.sort_values(by=['Score'], ascending=False)
-    selected = st.selectbox("Choose a FD to see his table.  (**Determinants -> Dependant: Score**)",rows['Determinants']+" -> "+rows['Dependant']+": "+rows['Score'].astype(str))
-    # get the attributes of the selected FD
-    attributes = selected.split(" -> ")[0].split(", ")
-    attributes.append(selected.split(" -> ")[1].split(": ")[0])
+    selected = st.selectbox("Choose a FD to see his table normalized.  (**Determinants -> Dependants: Score**)",rows['Determinants']+" -> "+rows['Dependant']+": "+rows['Score'].astype(str))
+    # get all the attributes of the selected FD based on the dataframe columns
+    attributes = []
+    for attr in df.columns:
+        if attr in selected:
+            attributes.append(attr)
     
     output = df[attributes]
     output = output.drop_duplicates()
