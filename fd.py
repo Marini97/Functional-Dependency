51c0db7c51a36f0c90b5e38076d7204a5f074804
Valerio Marini <marinivalerio97@gmail.com>
2023-03-31 11:36:26 +0200

    added doc strings

--- fd.py
+++ fd.py
@@ -5,68 +5,129 @@ import subprocess
 import jsonlines
 from io import StringIO
 from PIL import Image
-# to run this app, run the following command in the terminal
-# streamlit run fd.py
 
-# cache the uploaded dataset to avoid reading it every time the app is updated
+
 @st.cache_data
-def get_data(file):
-    return pd.read_csv(file)
+def get_data(file: str, header: bool=True) -> pd.DataFrame:
+    """Read the data file and return a dataframe.
+
+    Args:
+        file : str
+            File path of the data file.
+        header : bool, optional
+            True if the file has a header, False otherwise.
+
+    Returns:
+        df : pd.DataFrame
+            A pandas dataframe with the data read from the file.
+    """
+    if header:
+        df = pd.read_csv(file)
+    else:
+        df = pd.read_csv(file, header=None)
+    return df
 
 # the dataset parameter is used to check if the dataset has been changed
 # if it has been changed, then we need to read the output file again
 # if it hasn't been changed, then we can use the cached results
 @st.cache_data
-def read_output(dataset):
-    fd = True
+def read_json(dataset: str, df_cols: int, results: pd.DataFrame) -> pd.DataFrame:
+    """Read the output file from Metanome and return a dataframe with the results.
+
+    Args:
+        dataset : str
+            The name of the dataset.
+        df_cols : int
+            The number of columns in the dataset.
+        results : pd.DataFrame
+            The dataframe with the results.
+
+    Returns:
+        results : pd.DataFrame
+            A pandas dataframe with the results.
+    """
     try:
-        with jsonlines.open('results/data_fds') as reader:
-            df_num = len(df.columns)
-            for data in reader:
+        # read the output file from Metanome
+        with jsonlines.open('results/data_fds') as lines:
+            for line in lines:
                 
-                determinant = data['determinant']['columnIdentifiers']
-                for i in range(len(determinant)):
-                    determinant[i] = determinant[i]['columnIdentifier']
+                # get the determinants and dependant attributes
+                determinants = line['determinant']['columnIdentifiers']
+                for i in range(len(determinants)):
+                    determinants[i] = determinants[i]['columnIdentifier']
                     
-                dependant = data['dependant']['columnIdentifier']
+                dependant = line['dependant']['columnIdentifier']
                 
-                if len(determinant)+1 < df_num:
-                    score = df[determinant+[dependant]].drop_duplicates().shape[0]
-                    results.append({'determinants': determinant, 'dependant': dependant, 'score': score})
-        
+                # if the union of Determinants and Dependant contains all attributes, then the FD is filtered out
+                if len(determinants)+1 < df_cols:
+                    score = df[determinants+[dependant]].drop_duplicates().shape[0]
+                    row = {'Determinants': [determinants], 'Dependant': [dependant], 'Score': [score]}
+                    results.loc[len(results)] = row
+                    
     except Exception as exception:
-        fd = False
-
+        pass
     if len(results) == 0:
-        fd = False
-        
-    return results, fd
+        return []
+    return results
 
-# get all FDs and group them by Determinants
+
 @st.cache_data
-def get_fds(results, df_num):
+def get_fds(results: pd.DataFrame, df_cols: int) -> pd.DataFrame:
+    """This function takes the results of the FD algorithm and returns a dataframe with the FDs as columns and the attributes as rows.
+
+    Args:
+        results : pd.DataFrame
+            The results of the FD algorithm.
+        df_cols : int
+            The number of columns in the dataframe.
+
+    Returns:
+        rows : pd.DataFrame
+            A dataframe with Determinants, Dependant and Score as attributes and FDs as rows.
+    """
+    # Create a new dataframe with the columns 'Determinants', 'Dependant' and 'Score'
     rows = pd.DataFrame(columns=['Determinants', 'Dependant', 'Score'])
-    for result in results:
-        determinants = ", ".join(map(str,result['determinants']))
-        dependant = result['dependant']
-        score = result['score']
-        row = pd.DataFrame({'Determinants': [determinants], 'Dependant': [dependant], 'Score': [score]})
-        rows = pd.concat([rows, row], ignore_index=True)
+
+    # Loop through all results
+    for result in results.itertuples():
+        determinants = ""
         
-    # group the FDs by Determinants and concatenate the Dependant attributes
+        for r in result.Determinants[0]:
+            determinants = determinants + str(r) + ", "
+        determinants = determinants[:-2]
+        
+        dependant = str(result.Dependant[0])
+        score = str(result.Score[0])
+        
+        # Create a new row with the data of this result
+        row = {'Determinants': determinants, 'Dependant': dependant, 'Score': score}
+        
+        # Append the row to the dataframe
+        rows.loc[len(rows)] = row
+
+    # Group the dataframe by the determinants
     rows = rows.groupby('Determinants').agg({'Dependant': ', '.join, 'Score': 'mean'}).reset_index()
-    # group the FDs by subsets of Determinants and concatenate the Dependant attributes
+
+    # Loop through all rows of the dataframe
     for row in rows.itertuples():
+        # Loop through all rows of the dataframe again
         for row2 in rows.itertuples():
+            # If the determinants of row2 are a subset of the determinants of row
             if row.Index != row2.Index and set(row2.Determinants.split(", ")).issubset(set(row.Determinants.split(", "))):
+                # Add the dependants of row2 to the dependants of row
                 rows.loc[row.Index, 'Dependant'] = rows.loc[row.Index, 'Dependant']+", "+row2.Dependant
+                
+                # Remove row2 from the dataframe
                 rows = rows.drop(row2.Index)
-    
-    # if the union of Determinants and Dependant contains all attributes, then the FD is filtered out
+            
+    # Remove all rows with more than df_cols columns
     for row in rows.itertuples():
-        if len(row.Determinants.split(", "))+len(row.Dependant.split(", ")) >= df_num:
+        if len(row.Determinants.split(", "))+len(row.Dependant.split(", ")) >= df_cols:
             rows = rows.drop(row.Index)
+            
+    # Return the dataframe
     return rows
+
 
 icon = Image.open('icon.png')
 st.set_page_config(
@@ -85,13 +146,12 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
     
-# title of the app
+# title of the app and the icon
 col1, col2 = st.columns([2, 0.7])
-with col1:
-    st.title("Functional Dependency App")
-with col2:
-    st.image(icon, width=80)
-    
+col1.title("Functional Dependency App")
+col2.image(icon, width=80)
+
+# sidebar to select a dataset
 with st.sidebar:
     st.title("Select a Dataset")
     # select a dataset
@@ -101,23 +161,27 @@ with st.sidebar:
 
     # upload a dataset
     uploaded_file = st.file_uploader("Or upload a CSV file from your computer.")
+    header = True
+    
     if uploaded_file is not None and not uploaded_file.name.endswith(".csv"):
         st.error("You need to select a CSV file.")
         uploaded_file = None
     if uploaded_file is not None:
         try:
-            df = get_data(file=uploaded_file)
+            # select if header is present in the dataset
+            header = st.checkbox("Does the dataset have a header?", value=True)
+            df = get_data(uploaded_file, header)
             dataset = uploaded_file.name
         except(pd.errors.EmptyDataError):
             st.error("The selected CSV file is empty.")
             uploaded_file = None
-            
+   
 # write the selected dataset to a csv file 
 if df is not None:
     # remove all files from the results folder
     subprocess.run(["rm", "-r", "results/*"])
     # write the dataset to a csv file
-    df.to_csv("results/data.csv", index=False)
+    df.to_csv("results/data.csv", index=False, header=header)
     st.header("Selected Dataset")
     st.dataframe(df, use_container_width=True)
     
@@ -127,22 +191,22 @@ subprocess.run(["java", "-cp", "jars/*", "de.metanome.cli.App", "--algorithm", "
                 "--file-key", "INPUT_GENERATOR", "--files", "results/data.csv", "--separator", ",", 
                 "--header", "-o", "file:data"])
 
-results = []
-results, fd = read_output(dataset)
-
-df_num = len(df.columns)
-rows = get_fds(results, df_num)    
+df_cols = len(df.columns)
+df_rows = df.shape[0]
+results = pd.DataFrame(columns=['Determinants', 'Dependant', 'Score'])
+results = read_json(dataset, df_cols, results)
+rows = get_fds(results, df_cols)
 
 st.header("Functional Dependencies")
 # write the results to the streamlit app 
-if fd and rows.shape[0] > 0:
+if rows.shape[0] > 0:
     st.write("In the following dropdown menu you can see all the **Functional Dependencies**  in the dataset. " 
-             +"The **Functional Dependencies** are grouped by **Determinants** and sorted by their unique number of rows. "
-             +"A FD is filtered out if the union of Determinants and Dependants **contains all the attributes**.")
+             +"The **Functional Dependencies** are grouped by **Determinants** and sorted by their unique number of rows. ")
     
-    st.success("The dataset has **"+str(df_num)+" attributes** and **"+str(df.shape[0])+" rows**. " 
-             +"The HyFD Algorithm found "+str(len(results))+" **Functional Dependencies**.")
+    st.success("The dataset has **"+str(df_cols)+" attributes** and **"+str(df_rows)+" rows**. " 
+             +"The HyFD Algorithm has found "+str(len(results))+" **Functional Dependencies**.")
     
+    st.write("An FD is filtered out if the union of Determinants and Dependants **contains all the attributes**.")
     # sort the FDs by score
     rows = rows.sort_values(by=['Score'], ascending=False)
     selected = st.selectbox("Choose a FD to see the dataset normalized.  (**Determinants -> Dependants: Score**)",rows['Determinants']+" -> "+rows['Dependant']+": "+rows['Score'].astype(str))
@@ -164,6 +228,7 @@ if fd and rows.shape[0] > 0:
     output2 = df[attr1]
     output2 = output2.drop_duplicates()
     col1, col2 = st.columns(2)
+    
     col1.write("Table without the FD's Dependants attributes:")
     col1.dataframe(output1, use_container_width=True)
     col2.write("Table for the selected FD:")
